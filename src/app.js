99c3b86dbace89e5b5c25ee3c84e56c5c33960cb
Julutus <julian94info@gmail.com>
2024-10-07 10:33:44 +0000

    La llamada del dominio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,21 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const generateDomain = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".es", ".eu"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let d = 0; d < domain.length; d++) {
+          console.log(pronoun[p] + adj[a] + noun[n] + domain[d]);
+        }
+      }
+    }
+  }
+};
+console.log(generateDomain());
